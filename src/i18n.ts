26e6f299b7ba8324b145bd4949e9d7f63d1786d2
Yousief Sameh <yousief.sameh@outlook.com>
2025-02-01 00:46:55 +0200

    Make Changes On I18n File

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,30 +1,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi from "i18next-http-backend";
+import Backend from "i18next-http-backend";
 
 i18n
-	.use(LanguageDetector)
+	.use(Backend)
 	.use(initReactI18next)
-	.use(HttpApi)
 	.init({
 		fallbackLng: "ar",
-		interpolation: {
-			escapeValue: false,
-		},
-		detection: {
-			order: [
-				"cookie",
-				"htmlTag",
-				"querystring",
-				"localStorage",
-				"sessionStorage",
-				"navigator",
-				"path",
-				"subdomain",
-			],
-			caches: ["cookie"],
-		},
+		debug: true,
 		backend: {
 			loadPath: "/locales/{{lng}}/{{ns}}.json",
 			addPath: "/locales/{{lng}}/{{ns}}.missing.json",
@@ -37,6 +20,9 @@ i18n
 				credentials: "same-origin",
 				cache: "default",
 			},
+		},
+		interpolation: {
+			escapeValue: false,
 		},
 	});
 
