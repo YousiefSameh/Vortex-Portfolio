6681466386705b039222114b1e4a100cce11ff4b
Yousief Sameh <yousief.sameh@outlook.com>
2025-02-01 01:03:22 +0200

    Fix The Language Bug

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,29 +1,26 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: ["en", "ar"],
+    fallbackLng: "ar",
     debug: true,
-    defaultNS: false,
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json",
-      addPath: "/locales/{{lng}}/{{ns}}.missing.json",
-      allowMultiLoading: false,
-      crossDomain: false,
-      withCredentials: false,
-      overrideMimeType: false,
-      requestOptions: {
-        mode: "cors",
-        credentials: "same-origin",
-        cache: "default",
-      },
     },
+    ns: ["header", "hero", "services", "projects", "contact", "footer"],
+    defaultNS: "header",
     interpolation: {
       escapeValue: false,
+    },
+    detection: {
+      order: ["querystring", "cookie", "localStorage", "navigator"],
+      caches: ["localStorage", "cookie"],
     },
   });
 
