baf189d6a7a30f1e0a9cfb1fd8a20293c77f3697
Yousief Sameh <yousief.sameh@outlook.com>
2025-02-01 00:51:31 +0200

    Fix Some Bugs

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,27 +3,28 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 
 i18n
-	.use(Backend)
-	.use(initReactI18next)
-	.init({
-		fallbackLng: "ar",
-		debug: true,
-		backend: {
-			loadPath: "/locales/{{lng}}/{{ns}}.json",
-			addPath: "/locales/{{lng}}/{{ns}}.missing.json",
-			allowMultiLoading: false,
-			crossDomain: false,
-			withCredentials: false,
-			overrideMimeType: false,
-			requestOptions: {
-				mode: "cors",
-				credentials: "same-origin",
-				cache: "default",
-			},
-		},
-		interpolation: {
-			escapeValue: false,
-		},
-	});
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: ["en", "ar"],
+    debug: true,
+    defaultNS: false,
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json",
+      addPath: "/locales/{{lng}}/{{ns}}.missing.json",
+      allowMultiLoading: false,
+      crossDomain: false,
+      withCredentials: false,
+      overrideMimeType: false,
+      requestOptions: {
+        mode: "cors",
+        credentials: "same-origin",
+        cache: "default",
+      },
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
